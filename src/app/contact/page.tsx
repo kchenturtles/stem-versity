8e9eb224a70cca0db422b913eea500e0961b7a82
kchenturtles <94144671+kchenturtles@users.noreply.github.com>
2024-07-07 03:25:47 -0400

    contact updates

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Image from "next/image";
-
 import React from "react";
 import {
   Button,
@@ -13,38 +12,10 @@ import {
   IconButton
 } from "@material-tailwind/react";
 import { ArrowRightIcon, PhoneIcon, EnvelopeIcon, TicketIcon } from "@heroicons/react/24/outline";
-import BlogPostCard from "@/components/blog-post-card";
-
-const BLOG_POSTS = [
-  {
-    img: "/image/blogs/blog-1.png",
-    tag: "minority",
-    title: "Hydrogen-Powered Vehicles",
-    desc: "This article delves into the cutting-edge technology behind hydrogen fuel cells and their environmental benefits.",
-    author: { name: "Emma Roberts", img: "/image/avatar1.jpg"},
-    date: "July 15, 2021"
-  },
-  {
-    img: "/image/blogs/blog-2.png",
-    title: "Mental Health in the Digital Age",
-    tag: "minority",
-    desc: "This article explores the intricate relationship between social media usage and mental health",
-    author: { name: "Emma Roberts", img: "/image/avatar1.jpg"},
-    date: "July 15, 2021"
-  },
-  {
-    img: "/image/blogs/blog-3.png",
-    title: "Mars Colonization and Beyond",
-    tag: "minority",
-    desc: "This article takes readers on a journey through the latest developments in space exploration.",
-    author: { name: "Emma Roberts", img: "/image/avatar1.jpg"},
-    date: "July 15, 2021"
-  },
-];
 
 function Contact() {
   return (
-    <container>
+  <div>
     <header className="bg-gray-900 mb-96">
       <div className="container mx-auto px-8 h-[22rem] lg:px-48 translate-y-64">
         <Image
@@ -56,35 +27,26 @@ function Contact() {
         />
         <div className="flex mt-16 justify-between">
           <Typography variant="h5" className="text-3xl">
-            Emma Roberts
+            Kendree C
           </Typography>
-          <Button color="gray">follow</Button>
+          <Button color="gray"><a href = "mailto:kendree.chen@gmail.com">Connect</a></Button>
         </div>
         <div className="flex items-center gap-6">
           <div className="flex items-center gap-2 mt-3">
-            <Typography className="!text-gray-900 font-bold">323</Typography>
+            <Typography className="!text-gray-900 font-bold">President, </Typography>
             <Typography className="!text-gray-500 font-normal">
-              Posts
+              Greater Boston STEM Program
             </Typography>
           </div>
           <div className="flex items-center gap-2 mt-3">
-            <Typography className="!text-gray-900 font-bold">3.5k</Typography>
+            <Typography className="!text-gray-900 font-bold">Captain, </Typography>
             <Typography className="!text-gray-500 font-normal">
-              Followers
-            </Typography>
-          </div>
-          <div className="flex items-center gap-2 mt-3">
-            <Typography className="!text-gray-900 font-bold">260</Typography>
-            <Typography className="!text-gray-500 font-normal">
-              Following
+              FIRST Robotics Team 246
             </Typography>
           </div>
         </div>
         <Typography variant="lead" className="!text-gray-500 mt-8">
-          A wordsmith who believes in the power of language to shape our world,
-          inspire change, and connect us all. I bring a unique perspective to
-          the writing, blending the knowledge and experiences into
-          thought-provoking narratives.
+          {"Woman in STEM; educator; diversity advocate; public policy enthusiast; avid community engager; crocheter; cat mom"}
         </Typography>
         <Button
           variant="text"
@@ -99,57 +61,20 @@ function Contact() {
         </Button>
       </div>
     </header>
-    <section className="py-40 px-8">
-      <div className="container mx-auto mb-12">
-        <Typography variant="h3" color="blue-gray">
-          Check my latest blog posts
-        </Typography>
-      </div>
-      <div className="container mx-auto grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-4">
-        {BLOG_POSTS.map((props, idx) => (
-          <BlogPostCard key={idx} {...props} />
-        ))}
-        <Card
-          className="relative grid h-full w-full place-items-center overflow-hidden
-            bg-black"
-        >
-          <div className="absolute inset-0 h-full w-full bg-gray-900/75" />
-          <CardBody className="relative w-full">
-            <Typography variant="h3" className="mt-4" color="white">
-              Discover all my articles
-            </Typography>
-            <Typography color="white" className="py-4 font-normal">
-              I am a versatile writer who explores a wide range of genres and
-              topics.
-            </Typography>
-            <Button
-              variant="text"
-              color="white"
-              className="flex items-center gap-2"
-            >
-              read more
-              <ArrowRightIcon
-                strokeWidth={3}
-                className="h-3.5 w-3.5 text-white"
-              />
-            </Button>
-          </CardBody>
-        </Card>
-      </div>
-    </section>
-    <section className="px-8 pt-20">
+    <div className="px-8 pt-20">
     <div className="container mx-auto mb-5 md:mb-20 text-center">
-      <Typography variant="h1" color="blue-gray" className="mb-4">
-        Say Hi!
+      <Typography variant="h2" color="blue-gray" className="mb-4">
+        Thoughts? Questions?
       </Typography>
       <Typography variant="lead" className="mx-auto !text-gray-500">
-        Any questions or remarks? Just write us a messaage!
+        I'd love to hear from you!
       </Typography>
+    </div>
     </div>
     <div className="container mx-auto">
       <Card shadow={true} className="border border-gray/50">
         <CardBody className="grid grid-cols-1 md:p-10 lg:grid-cols-2 md:gap-28">
-          <div className="w-full mt-8 md:mt-0 md:px-10 h-full p-5">
+          <div className="w-full mt-2 md:mt-0 md:px-10 h-full p-5">
             <form action="#">
               <div className="mb-8 grid gap-4 lg:grid-cols-2">
                 {/* @ts-ignore */}
@@ -215,8 +140,7 @@ function Contact() {
               variant="lead"
               className="mx-auto mb-8 text-base !text-gray-500"
             >
-              Fill up the form and our Team will get back to you within 24
-              hours.
+              Feel free to leave me your thoughts! I'd love to hear from you.
             </Typography>
             <div className="flex gap-5">
               <PhoneIcon className="h-6 w-6 text-white" />
@@ -238,21 +162,20 @@ function Contact() {
             </div>
             <div className="flex items-center gap-5">
               <IconButton variant="text" color="white">
-                <i className="fa-brands fa-facebook text-lg" />
+              <a href = "https://www.linkedin.com/in/kendree-chen/"><i className="fa-brands fa-linkedin text-lg" /></a>
               </IconButton>
-              <IconButton variant="text" color="white">
-                <i className="fa-brands fa-instagram text-lg" />
+              <IconButton variant="text" color="white" >
+              <a href = "https://www.instagram.com/kendree.c/"><i className="fa-brands fa-instagram text-lg" /></a>
               </IconButton>
-              <IconButton variant="text" color="white">
-                <i className="fa-brands fa-github text-lg" />
+              <IconButton variant="text" color="white" >
+                <a href = "github.com/kchenturtles"><i className="fa-brands fa-github text-lg" /></a>
               </IconButton>
             </div>
           </div>
         </CardBody>
       </Card>
     </div>
-  </section>
-</container>
+</div>
   );
 }
 export default Contact;
