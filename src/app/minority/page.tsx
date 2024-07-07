e03160e2023b142262954939841df5002e864dfb
kchenturtles <94144671+kchenturtles@users.noreply.github.com>
2024-07-07 03:26:06 -0400

    fix page

--- src/app/minority/page.tsx
+++ src/app/minority/page.tsx
@@ -1,143 +1,68 @@
-"use client";
-
 import React from "react";
-import {
-  Button,
-  Typography,
-  Tabs,
-  TabsHeader,
-  Tab,
-} from "@material-tailwind/react";
-import { ArrowSmallDownIcon } from "@heroicons/react/24/solid";
 import BlogPostCard from "@/components/blog-post-card";
+import { getAllPosts } from "@/lib/api";
+import { LatestPostsCards } from "../latest-posts-cards";
+import Image from "next/image";
+
+export const metadata = {
+  title: "Minority Representation in STEM",
+};
 
 
-const POSTS = [
-  {
-    img: `/image/blogs/blog2.svg`,
-    tag: "Enterprise",
-    title: "The key new features and changes in Tailwind CSS",
-    desc: "Don't be scared of the truth because we need to restart the human foundation in truth And I love you like Kanye loves Kanye I love Rick Owens bed design but the back is too high for the beams and angle of the ceiling I also wanted to point out.",
-    date: "10 September 2022",
-    author: {
-      img: `/image/avatar1.jpg`,
-      name: "Ryan Samuel",
-    },
-  },
-  {
-    img: `/image/blogs/blog6.svg`,
-    tag: "Startups",
-    title: "Lyft launching cross-platform service this week",
-    desc: "Don't be scared of the truth because we need to restart the human foundation in truth And I love you like Kanye loves Kanye I love Rick Owens bed design but the back is too high for the beams and angle of the ceiling I also wanted to point out.",
-    date: "12 September 2022",
-    author: {
-      img: `/image/blogs/blog2.svg`,
-      name: "Nora Hazel",
-    },
-  },
-  {
-    img: `/image/blogs/blog3.svg`,
-    tag: "Trending",
-    title: "6 insights into the French Fashion landscape",
-    desc: "Don't be scared of the truth because we need to restart the human foundation in truth And I love you like Kanye loves Kanye I love Rick Owens bed design but the back is too high for the beams and angle of the ceiling I also wanted to point out.",
-    date: "16 September 2022",
-    author: {
-      img: `/image/avatar2.jpg`,
-      name: "Otto Gonzalez",
-    },
-  },
-  {
-    img: `/image/blogs/blog4.svg`,
-    tag: "Lifestyle",
-    title: "Autodesk looks to future of 3D printing with Project",
-    desc: "Don't be scared of the truth because we need to restart the human foundation in truth And I love you like Kanye loves Kanye I love Rick Owens bed design but the back is too high for the beams and angle of the ceiling I also wanted to point out.",
-    date: "18 September 2022",
-    author: {
-      img: `/image/avatar3.jpg`,
-      name: "Ryan Samuel",
-    },
-  },
-  {
-    img: `/image/blogs/blog5.svg`,
-    tag: "Enterprise",
-    title: "Autodesk looks to future of 3D printing with Project",
-    desc: "Don't be scared of the truth because we need to restart the human foundation in truth And I love you like Kanye loves Kanye I love Rick Owens bed design but the back is too high for the beams and angle of the ceiling I also wanted to point out.",
-    date: "10 September 2022",
-    author: {
-      img: `/image/avatar3.jpg`,
-      name: "Ryan Samuel",
-    },
-  },
-  {
-    img: `/image/blogs/blog6.svg`,
-    tag: "Startups",
-    title: "Lyft launching cross-platform service this week",
-    desc: "Don't be scared of the truth because we need to restart the human foundation in truth And I love you like Kanye loves Kanye I love Rick Owens bed design but the back is too high for the beams and angle of the ceiling I also wanted to point out.",
-    date: "12 September 2022",
-    author: {
-      img: `/image/avatar2.jpg`,
-      name: "Nora Hazel",
-    },
-  },
-];
-
-export function Posts() {
+export function Minority() {
+  const posts = getAllPosts();
   return (
-    <section className="grid min-h-screen place-items-center p-8">
-      <Tabs value="trends" className="mx-auto max-w-7xl w-full mb-16 ">
-        <div className="w-full flex mb-8 flex-col items-center">
-          <TabsHeader className="h-10 !w-12/12 md:w-[50rem] border border-white/25 bg-opacity-90">
-            <Tab value="trends">Trends</Tab>
-            <Tab value="frontend">Frontend</Tab>
-            <Tab value="backend">Backend</Tab>
-            <Tab value="cloud">Cloud</Tab>
-            <Tab value="ai">AI</Tab>
-            <Tab value="tools">Tools</Tab>
-          </TabsHeader>
+    <div>
+    <header className="grid !min-h-[24rem] bg-gray-900 px-8">
+        <div className="container mx-auto mt-12 w-full lg:mt-64 ">
+        <div className="mx-2 lg:mx-4 -mb-32 rounded-xl bg-white p-5 md:p-14 shadow-md">
+          <h1 className = "y-2 w-full font-bold text-5xl !text-gray-900">
+              Minority Representation in STEM
+            </h1>
+            <div className="block antialiased font-sans text-xl font-normal leading-relaxed text-inherit mx-auto mt-8 w-full !text-gray-900">
+            Science, Technology, Engineering, and Math (STEM) is the future. Innovation in STEM drives advancements in healthcare, technology, sustainability, and beyond, directly impacting the quality of life for people worldwide. Diversity in STEM is crucial to address the full range of issues that affect society today.  But statistics show that diversity remains an issue among students in STEM. How do we address this issue?
+            </div>
+            <div className="block antialiased font-sans text-md leading-relaxed text-inherit mx-auto w-full !text-gray-900 font-bold mt-12">
+            Read More
+            </div>
+            <div className="flex flex-col gap-2 md:mb-2 md:w-10/12 md:flex-row">
+            <button
+            className="flex items-center gap-2 mt-2 !text-gray-50 bg-gray-900 px-4 py-2 rounded-lg hover:shadow-lg transition duration-300 ease-in-out">
+                Research
+              </button>
+              <button
+                className="flex items-center gap-2 mt-2 !text-gray-50 bg-gray-900 px-4 py-2 rounded-lg hover:shadow-lg transition duration-300 ease-in-out">
+                Action
+              </button>
+            </div>
+          </div>
         </div>
-      </Tabs>
-      <Typography variant="h6" className="mb-2">
-        Latest Blog Posts
-      </Typography>
-      <Typography variant="h1" className="mb-2">
-        Trends News
-      </Typography>
-      <Typography
-        variant="lead"
-        color="gray"
-        className="max-w-3xl mb-36 text-center text-gray-500"
+      </header>
+    <div className="grid min-h-screen place-items-center p-8 mt-48">
+    <div className = "text-center block antialiased font-sans text-base text-xl leading-relaxed text-blue-gray-900 mb-2 font-bold uppercase">Research</div>
+    <div className="text-center block antialiased font-sans text-lg font-normal leading-relaxed text-inherit mx-auto w-full !text-gray-500 lg:w-11/12 lg:px-8">
+      Check out what&apos;s new in the web development and tech world! Do not
+      forget to subscribe to our blog and we will notify you with the latest
+      news.
+    </div>
+        <LatestPostsCards tag={"minority"} type={"research"} length = {3}/>
+    </div>
+    <div className="grid min-h-screen place-items-center p-8 mt-6">
+    <div className = "text-center block antialiased font-sans text-base text-xl leading-relaxed text-blue-gray-900 mb-2 font-bold uppercase">How To Take Action</div>
+    <div className="text-center block antialiased font-sans text-lg font-normal leading-relaxed text-inherit mx-auto w-full !text-gray-500 lg:w-11/12 lg:px-8">
+      Check out what&apos;s new in the web development and tech world! Do not
+      forget to subscribe to our blog and we will notify you with the latest
+      news.
+    </div>
+        <LatestPostsCards tag={"minority"} type={"blog"} length = {3}/>
+      <button
+        className="flex items-center gap-2 mt-24 text-gray-50 bg-gray-900 px-4 py-2 rounded-lg shadow-lg hover:shadow-xl transition duration-300 ease-in-out"
       >
-        Check out what&apos;s new in the web development and tech worls! Do not
-        forget to subscribe to our blog and we will notify you with the latest
-        news.
-      </Typography>
-      <div className="container my-auto grid grid-cols-1 gap-x-8 gap-y-16 items-start lg:grid-cols-3">
-        {POSTS.map(({ img, tag, title, desc, date, author }) => (
-          <BlogPostCard
-            key={title}
-            img={img}
-            tag={tag}
-            title={title}
-            desc={desc}
-            date={date}
-            author={{
-              img: author.img,
-              name: author.name,
-            }}
-          />
-        ))}
-      </div>
-      <Button
-        variant="text"
-        size="lg"
-        color="gray"
-        className="flex items-center gap-2 mt-24"
-      >
-        <ArrowSmallDownIcon className="h-5 w-5 font-bold text-gray-900" />
-        VIEW MORE
-      </Button>
-    </section>
+        READ MORE
+      </button>
+    </div>
+    </div>
   );
 }
 
-export default Posts;
+export default Minority;
