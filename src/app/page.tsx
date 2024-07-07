0b3c75e4d36007d15b3f601d64dd8cbab4a7d37b
kchenturtles <94144671+kchenturtles@users.noreply.github.com>
2024-07-07 03:25:21 -0400

    layout update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,23 @@
 // sections
-import Hero from "./hero";
-import VideoIntro from "./video-intro";
-import Feature from "./feature";
-import MobileConvenience from "./mobile-convenience";
-import Testimonials from "./testimonials";
-import Faqs from "./faqs";
+import Hero  from "./hero";
+import  { LatestPosts } from "./latest-posts";
+import Feature  from "./feature";
 
-export default function Campaign() {
+export default function Main() {
   return (
     <>
       <Hero />
-      <VideoIntro />
       <Feature />
-      <Footer />
+      <div className = "container mx-auto">
+      <div className = "text-center block antialiased font-sans text-base leading-relaxed text-blue-gray-900 mb-2 font-bold uppercase">Read The Latest</div>
+      <h1 className = "text-center y-2 w-full font-bold text-5xl !text-neutral-900">Recent Posts & Articles</h1>
+      <div className="text-center block antialiased font-sans text-xl font-normal leading-relaxed text-inherit mx-auto w-full !text-gray-500 lg:w-11/12 lg:px-8">
+        Check out what&apos;s new in the web development and tech world! Do not
+        forget to subscribe to our blog and we will notify you with the latest
+        news.
+      </div>
+      </div>
+      <LatestPosts tag={""} type = {""} length = {3}/>
     </>
   );
 }
