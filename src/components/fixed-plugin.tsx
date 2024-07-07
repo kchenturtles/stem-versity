e2888b4e8f2d155cad4e1ff3c25a2d5e433c6788
kchenturtles <94144671+kchenturtles@users.noreply.github.com>
2024-07-07 03:24:10 -0400

    fix plugin

--- src/components/fixed-plugin.tsx
+++ src/components/fixed-plugin.tsx
@@ -4,7 +4,7 @@ import { Button } from "@material-tailwind/react";
 
 export function FixedPlugin() {
   return (
-    <a href="https://www.material-tailwind.com" target="_blank">
+    <a href="https://www.pewresearch.org/" target="_blank">
       <Button
         color="white"
         size="sm"
@@ -14,10 +14,10 @@ export function FixedPlugin() {
           width={128}
           height={128}
           className="w-5 h-5"
-          alt="Material Tailwind"
-          src="https://www.material-tailwind.com/favicon.png"
+          alt="Pew Research Center"
+          src="/pew-research-center.png"
         />{" "}
-        Made With Material Tailwind
+        Explore Census Data
       </Button>
     </a>
   );
