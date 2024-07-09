4c144b8cdfd38a0ac86ce64835f5c8820eff91ca
kchenturtles <94144671+kchenturtles@users.noreply.github.com>
2024-07-09 02:31:13 -0400

    fix blog pages

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -33,10 +33,10 @@ import {
       <header className="grid !min-h-[24rem] bg-gray-900 px-8">
       <div className="container mx-auto mt-12 w-full lg:mt-64 ">
       <div className="mx-2 lg:mx-4 -mb-32 rounded-xl bg-white p-5 md:p-14 shadow-md">
-        <h1 className = "y-2 w-full font-bold text-5xl !text-gray-900">
+        <h1 className = "y-2 w-full font-bold text-3xl !text-gray-900">
             {getPost.title}
           </h1>
-          <div className="block antialiased font-sans text-xl font-normal leading-relaxed text-inherit mx-auto mt-8 w-full !text-gray-900">
+          <div className="block antialiased font-sans text-md font-normal leading-relaxed text-inherit mx-auto mt-8 w-full !text-gray-700">
           {getPost.summary}
           </div>
           <div className="block antialiased font-sans text-md leading-relaxed text-inherit mx-auto w-full !text-gray-900 font-bold mt-12">
@@ -44,12 +44,12 @@ import {
           </div>
           <div className="flex flex-col gap-2 md:mb-2 md:w-10/12 md:flex-row">
           <button
-          className="flex items-center gap-2 mt-2 !text-gray-50 bg-gray-900 px-4 py-2 rounded-lg hover:shadow-lg transition duration-300 ease-in-out">
-              <a href = {`/${getPost.tag}`} > {getPost.tag}</a>
+          className="flex items-center gap-2 mt-2 !text-gray-500 px-1 py-2 hover:underline transition duration-300 ease-in-out">
+              <a href = {`/${getPost.tag}`} > #{getPost.tag}</a>
             </button>
             <button
-              className="flex items-center gap-2 mt-2 !text-gray-50 bg-gray-900 px-4 py-2 rounded-lg hover:shadow-lg transition duration-300 ease-in-out">
-              {getPost.type}
+              className="flex items-center gap-2 mt-2 !text-gray-500 px-1 py-2 transition duration-300 ease-in-out">
+              #{getPost.type}
             </button>
           </div>
         </div>
@@ -64,26 +64,6 @@ import {
           <ReactMarkdown
             className={styles["markdown"]}
             components={{
-              img: (props) => (
-                <span className={styles.imageWrapper}>
-                  <Image
-                    className="responsive-image"
-                    src={props.src as string}
-                    alt={props.alt as string}
-                  />
-                </span>
-              ),
-              h6: (props) => (
-                <div className={styles.button}>
-                  <Link
-                    href="https://trusted.bu.edu/s/1759/22/form.aspx?sid=1759&gid=2&pgid=3962&cid=7331&dids=359&bledit=1&appealcode=WEBBUA"
-                    target="_blank"
-                    className={`${styles.donateButton} ${styles.inlineDonateButton}`}
-                  >
-                    {" "}{props.children}{" "}
-                  </Link>
-                </div>
-              ),
               code: (props) => ( // I feel really bad for doing this but I kinda had no choice
                 <span className={styles.brandText}> {props.children} </span>
               ),
--- src/components/article-card.tsx
+++ src/components/article-card.tsx
@@ -18,13 +18,6 @@ function ArticleCard({ img, title, desc }: ArticleCardProps) {
         className="relative grid min-h-[30rem] items-end overflow-hidden rounded-xl"
         color="transparent"
       >
-        <Image
-          width={768}
-          height={768}
-          src={img}
-          alt="bg"
-          className="absolute inset-0 h-full w-full object-cover object-center"
-        />
         <div className="absolute inset-0 bg-black/60" />
         <CardBody className="relative flex flex-col justify-end">
           <Typography variant="h4" color="white">
