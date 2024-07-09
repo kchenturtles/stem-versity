dc430a3ec14e17811f9d07c3e859407069edf1ae
kchenturtles <94144671+kchenturtles@users.noreply.github.com>
2024-07-09 03:24:00 -0400

    styling fixes

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -30,32 +30,30 @@ import {
   
     return (
       <div className="mx-auto">
-      <header className="grid !min-h-[24rem] bg-gray-900 px-8">
-      <div className="container mx-auto mt-12 w-full lg:mt-64 ">
-      <div className="mx-2 lg:mx-4 -mb-32 rounded-xl bg-white p-5 md:p-14 shadow-md">
+      <Image src="/image/space.png" alt={"background"} width = {2000} height = {500} className = "w-full grid !min-h-[24rem] bg-gray-900 p-0 relative -mt-48"/>
+      <div className="container mx-auto -mt-64 absolute flex justify-center px-24">
+      <div className={styles.card}>
         <h1 className = "y-2 w-full font-bold text-3xl !text-gray-900">
             {getPost.title}
           </h1>
           <div className="block antialiased font-sans text-md font-normal leading-relaxed text-inherit mx-auto mt-8 w-full !text-gray-700">
           {getPost.summary}
           </div>
-          <div className="block antialiased font-sans text-md leading-relaxed text-inherit mx-auto w-full !text-gray-900 font-bold mt-12">
+          <div className="block antialiased font-sans text-md leading-relaxed text-inherit mx-auto w-full !text-gray-900 font-bold mt-8">
           {getPost.date}
           </div>
           <div className="flex flex-col gap-2 md:mb-2 md:w-10/12 md:flex-row">
-          <button
-          className="flex items-center gap-2 mt-2 !text-gray-500 px-1 py-2 hover:underline transition duration-300 ease-in-out">
+          <div
+          className="flex items-center gap-2 mt-1 !text-gray-500 px-1 py-2 hover:underline transition duration-300 ease-in-out">
               <a href = {`/${getPost.tag}`} > #{getPost.tag}</a>
-            </button>
-            <button
-              className="flex items-center gap-2 mt-2 !text-gray-500 px-1 py-2 transition duration-300 ease-in-out">
+            </div>
+            <div
+              className="flex items-center gap-2 mt-1 !text-gray-500 px-1 py-2 transition duration-300 ease-in-out">
               #{getPost.type}
-            </button>
+            </div>
           </div>
         </div>
-      </div>
-    </header>      
-      
+      </div>      
         <div className="mt-48">
           <Link href="/">
           </Link>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,11 +10,9 @@ export default function Main() {
       <Feature />
       <div className = "container mx-auto">
       <div className = "text-center block antialiased font-sans text-base leading-relaxed text-blue-gray-900 mb-2 font-bold uppercase">Read The Latest</div>
-      <h1 className = "text-center y-2 w-full font-bold text-5xl !text-neutral-900">Recent Posts & Articles</h1>
+      <h1 className = "text-center y-2 w-full font-bold text-4xl !text-neutral-900">Recent Posts & Articles</h1>
       <div className="text-center block antialiased font-sans text-xl font-normal leading-relaxed text-inherit mx-auto w-full !text-gray-500 lg:w-11/12 lg:px-8">
-        Check out what&apos;s new in the web development and tech world! Do not
-        forget to subscribe to our blog and we will notify you with the latest
-        news.
+        Check out what&apos;s new! 
       </div>
       </div>
       <LatestPosts tag={""} type = {""} length = {3}/>
