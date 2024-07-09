fde5372f929f911a9bd920c23bbea1c8a53a33f9
kchenturtles <94144671+kchenturtles@users.noreply.github.com>
2024-07-09 01:08:47 -0400

    fix more stuff

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import { Footer, Navbar } from "@/components";
 import { LatestPostsCards } from "../latest-posts-cards";
 
-export async function Blog() {
+function Blog() {
   const posts = getAllPosts();
   
   return (
